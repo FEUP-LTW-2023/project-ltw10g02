ae74281df8e45ad1f76b9487aa12346ecc627f1e
up202211693 <matheusutino@usp.br>
2023-04-06 16:56:21 +0100

    Change name layout.css to menu.css

--- templates/common.tpl.php
+++ templates/common.tpl.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/form.css">
-    <link rel="stylesheet" href="../css/layout.css">
+    <link rel="stylesheet" href="../css/menu.css">
   </head>
   <body>
     <header>
