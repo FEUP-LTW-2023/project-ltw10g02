5736d796222dcea7f565757c29b87d13b2b81345
up202211693 <up202211693@fe.up.pt>
2023-04-17 15:25:34 +0100

    Update register

--- actions/action_register.php
+++ actions/action_register.php
@@ -12,14 +12,14 @@
 
     User::addUser($db, $session, $_POST['name'], $_POST['username'], $_POST['password'], $_POST['password_repeated'], $_POST['email'], "client");
 
-    if($session->getMessages()['type'] === 'error'){
+    if($session->getMessages()['type'] === 'sucess')
+        header('Location: /../pages.login.php' );  // redirect to the page we came from
+    else{
         $session->addMessage('data', $_POST['name']);
         $session->addMessage('data', $_POST['username']);
         $session->addMessage('data', $_POST['email']);
         header('Location: /../pages/register.php');
-    }
-    else{
-        header('Location: /../pages/login.php'); 
+
     }
 
 ?>
