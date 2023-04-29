f5d825f8b13315124a774cfd40e739232a8e7499
mariaarabelo <mariaesrabelo@gmail.com>
2023-04-29 12:45:13 +0100

    Merge branch 'main' of https://github.com/FEUP-LTW-2023/project-ltw10g02

---
+++ actions/action_update_profile.php
@@ -0,0 +1,17 @@
+<?php
+//tudo errado por enquanto
+    declare(strict_types=1);
+
+    require_once(__DIR__ . '/../utils/session.php');
+    $session = new Session();
+
+    require_once(__DIR__ . '/../database/database_connection.php');
+
+    require_once(__DIR__ . '/../database/classes/user.php');
+
+    $db = getDatabaseConnection();
+
+    
+    header('Location: ../pages/profile.php'); 
+
+?>
