cf536b36900f996bf6a126bc135846c1961bad5b
mariaarabelo <mariaesrabelo@gmail.com>
2023-05-20 15:56:48 +0100

    $session parameter

--- templates/profile.tpl.php
+++ templates/profile.tpl.php
@@ -11,7 +11,7 @@
         <?php if (!empty($tickets)): ?>
             <article id = "user_tickets">
                 <h2>Last Tickets</h2>
-                <?php drawTickets($tickets, 'profile_tickets') ?>
+                <?php drawTickets($session, $tickets, 'profile_tickets') ?>
                 <button onclick="redirectToPage('../pages/my_tickets.php')">Show all tickets</button>
             </article>
         <?php endif; ?>
