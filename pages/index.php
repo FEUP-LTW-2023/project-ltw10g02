7a8ccc8a0bb84eb87ef4d58f95e53c9f2cbe0bfc
up202211693 <up202211693@fe.up.pt>
2023-05-21 09:44:44 +0100

    responsive user list page and remove index page with all tickets

--- pages/index.php
+++ pages/index.php
@@ -4,24 +4,11 @@
     require_once(__DIR__ . '/../utils/session.php');
     $session = new Session();
 
-    require_once __DIR__ . '/../database/database_connection.php';
-    require_once __DIR__ . '/../database/classes/department.php';
-    require_once __DIR__ . '/../database/classes/faq.php';
-    require_once __DIR__ . '/../database/classes/ticket.php';
-    require_once __DIR__ . '/../database/classes/user.php';
-
-    require_once(__DIR__ . '/../templates/common.tpl.php');
-    require_once(__DIR__ . '/../templates/tickets.tpl.php');
-
-
-    // Retrieve all tickets from the database
-    $db = getDatabaseConnection();
-    $tickets = Ticket::getAll($db);
-
-
-    // Display the tickets in an HTML table
-    drawHeader($session);
-    drawTickets($session, $tickets, 'index');
-    drawFooter();
+    if(!$session->isLoggedIn())
+        header("Location: pages/login.php");
+    else
+        header("Location: pages/profile.php");
+    
+    exit();
 
 ?>
